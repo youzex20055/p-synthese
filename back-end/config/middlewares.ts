fa2d5cf55e314819e3c5c7ef4796c994e8b31c42
youzex20055 <youssefhdilisse5@gmail.com>
2025-06-03 23:43:38 +0100

    frr

--- back-end/config/middlewares.ts
+++ back-end/config/middlewares.ts
@@ -7,10 +7,10 @@ export default [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'http://localhost:*', 'https://*'],
-          'img-src': ["'self'", 'data:', 'blob:', 'http://localhost:*', 'https://*'],
-          'media-src': ["'self'", 'data:', 'blob:', 'http://localhost:*', 'https://*'],
-          'default-src': ["'self'", 'https://*'],
+          'connect-src': ["'self'", 'http://localhost:*', 'https://*', 'http://*'],
+          'img-src': ["'self'", 'data:', 'blob:', 'http://localhost:*', 'https://*', 'http://*'],
+          'media-src': ["'self'", 'data:', 'blob:', 'http://localhost:*', 'https://*', 'http://*'],
+          'default-src': ["'self'", 'https://*', 'http://*'],
           upgradeInsecureRequests: null,
         },
       },
@@ -20,10 +20,11 @@ export default [
     name: 'strapi::cors',
     config: {
       enabled: true,
-      origin: ['http://localhost:3000', 'http://localhost:1337', 'https://*'],
+      origin: ['http://localhost:3000', 'http://localhost:1337', 'http://18.208.134.101:*', 'https://*', 'http://*'],
       headers: ['*'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       credentials: true,
+      keepHeaderOnError: true,
     },
   },
   'strapi::poweredBy',
